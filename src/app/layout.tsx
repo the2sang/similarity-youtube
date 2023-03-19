624f6dae5856b2c80efa96c4d0987f3f4a0a434b
KHC <the2sang@gmail.com>
2023-03-20 01:06:20 +0900

    6th commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,37 @@
+import Navbar from '@/components/Navbar'
+import { Toaster } from '@/components/ui/toast'
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
-import { cn } from '@/lib/utils'
-import Paragraph from '@/components/ui/Paragraph'
-import Providers from '@/components/Providers'
-import Navbar from '@/components/Navbar'
 
-const inter = Inter({subsets: ['latin']})
+import MobileMenu from '@/components/MobileMenu'
+import Providers from '@/components/Providers'
+import { cn } from '@/lib/utils'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en" className={cn('bg-white text-slate-900 antialiased', inter.className)}>
-      <body className='min-h-screen bg-slate-50 dark:bg-slate-900 antialiased'>
+    return (
+        <html
+            lang='en'
+            className={cn('bg-white text-slate-900 antialiased', inter.className)}>
+        <body className='min-h-screen bg-slate-50 dark:bg-slate-900 antialiased'>
         <Providers>
-           {children}
-           {/* <Navbar /> */}
+            {/* @ts-expect-error Server Component */}
+            <Navbar />
+            <Toaster position='bottom-right' />
+
+            <MobileMenu />
+
+            <main>{children}</main>
         </Providers>
-        
-        {/* Allow for more height on mobile devices */}
+
+        {/* Allow more height for mobile menu on mobile */}
         <div className='h-40 md:hidden' />
-      </body>
-    </html>
-  )
+        </body>
+        </html>
+    )
 }
